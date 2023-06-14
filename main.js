f4b63f21350ba87817919d11b639d7af57df6010
Paridhi Parajuli <paridhiparajuli@Paridhis-MacBook-Pro.local>
2023-06-14 12:55:35 -0500

    raised effect

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 
+let dropdown = document.getElementById("dropdown");
+console.log(dropdown);
+var mapContainer = document.getElementById('map');
+
+// Set the dimensions of the map container
+var containerWidth = mapContainer.clientWidth;
+var containerHeight = mapContainer.clientHeight;
+
+
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFyaWRoaTEyIiwiYSI6ImNsaWMxcnRwejBnYXkzZG1ub21xbmxjdWcifQ.xfiUnCHe2s0IX5NeJ0qSxQ';
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/streets-v12',
 center: [-100.486052, 37.830348],
-zoom: 3
+zoom: 2.5,
+width: containerWidth,
+height: containerHeight
 });
 
 // Global variables
@@ -57,6 +68,11 @@ fetch('svi_interactive_map.csv')
 console.error('error at loading svi csv ', error);
 });
 
+function minMax(rows){
+    minimum = 0
+    maximum =500
+}
+
 function searchCSV(rows, searchCounty,searchState) {
     /**
  * This is a function that searches data corresonding to given county and state from the csv.
@@ -70,6 +86,7 @@ function searchCSV(rows, searchCounty,searchState) {
     searchState = '"'+searchState +'"';
   let filteredData= null;
   let stateName = null;
+
   for (let i = 1; i < rows.length; i++) {
     const rowData = rows[i].split(',');
     if((rowData[8] === searchCounty) && (rowData[4]==searchState)) {
@@ -81,97 +98,140 @@ function searchCSV(rows, searchCounty,searchState) {
   }
 
 map.on('load', ()=> {
+    map.addSource('states', {
+        'type':'geojson',
+        'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
+    });
+      map.addLayer({
+    'id':'state-boundaries',
+    'type': 'line',
+    'source':'states',
+    'paint': {
+        'line-color':'black',
+        'line-width':0.4
+    }
+    });
+    map.addLayer({
+        id: 'state-base',
+        type: 'fill',
+        source: 'states',
+        paint: {
+          'fill-color': '#627BC1',
+          'fill-opacity': 0.1
+        }
+      });
+      
+      // Raised layer (with raised effect)
+      map.addLayer({
+        id: 'state-raised',
+        type: 'fill-extrusion',
+        source: 'states',
+        paint: {
+          'fill-extrusion-color': '#627BC1',
+          'fill-extrusion-height': 100,
+          'fill-extrusion-opacity': 0.8
+        },
+        filter: ['==', 'STATE_ID', ''] // Initially empty filter
+      });
+    });
+
+    map.on('click', 'state-base', (e) => {
+        if (e.features.length > 0) {
+          // Update the filter of the raised layer to show only the hovered polygon
+          map.setFilter('state-raised', ['==', 'STATE_ID', e.features[0].properties.STATE_ID]);
+        }
     
 
-  map.addSource('states', {
-    'type':'geojson',
-    'data': 'https://docs.mapbox.com/mapbox-gl-js/assets/us_states.geojson'
-});
-map.addLayer({
-  'id':'state-boundaries',
-  'type': 'line',
-  'source':'states',
-  'paint': {
-      'line-color':'#627BC1',
-      'line-width':1
-  }
+
+
+
+    map.on('click', 'state-raised',(e)=>{
+
+        const stateId = e.features[0].properties.STATE_ID
+        pp2= new mapboxgl.Popup({
+            className: 'custom-popup-small',
+            closeButton: false,
+            closeOnClick: false
+          });
+
+
+  // Filter the county geojson data to get the counties within the clicked state
+  const filteredCountyFeatures = countyGeojson.features.filter((feature) => {
+    return feature.properties.STATEFP === stateId;
   });
-  console.log("hi");
-// adding color to the states
-map.addLayer({
-  'id': 'state-area',
-  'type': 'fill',
-  'source': 'states',
-  'layout': {},
-  'paint': {
-  'fill-color': '#627BC1',
-  'fill-opacity': [
-  'case',
-  ['boolean', ['feature-state', 'hover'], false],
-  1,
-  0.05
-  ]
+
+  // Create a new geojson object for the filtered county features
+  const countyGeojsonData = {
+    type: 'FeatureCollection',
+    features: filteredCountyFeatures
+  };
+
+  if (map.getLayer('county-borders')) {
+    map.removeLayer('county-borders');
   }
+
+  // Remove the existing "county-borders" source if it exists
+  if (map.getSource('county-borders')) {
+    map.removeSource('county-borders');
+  }
+
+  // Add a new layer for the county boundaries
+  map.addLayer({
+    id: 'county-borderss',
+    type: 'line',
+    source: {
+      type: 'geojson',
+      data: countyGeojsonData
+    },
+    paint: {
+      'line-color': 'black',
+      'line-width': 0.8,
+      'line-opacity': 1
+    }
   });
-        
-});
+  map.addLayer({
+    id: 'county-area',
+    type: 'fill',
+    source: {
+      type: 'geojson',
+      data: countyGeojsonData
+    },
+    paint: {
+      'fill-color': 'black',
+      'fill-opacity': [
+      'case',
+      ['boolean', ['feature-state', 'hover'], false],
+      1,
+      0.1
+      ]
+      }
+  });
+  map.on("click", "county-area" , (event)=>{
+    pp = new mapboxgl.Popup({
+      className: 'custom-popup'
+    }).setLngLat(event.lngLat);
+    let result = searchCSV(rows, event.features[0].properties.NAME +' County' , event.features[0].properties.STATEFP );
+    pp.setHTML(`
+    <h4> ${event.features[0].properties.NAME }, ${result.st }</h4>
+    <p> SVI : ${result.svi}</p>
+  `).addTo(map);
+
+  });
+      });
+    });
+
+
+
 
 var pp1 = new mapboxgl.Popup({
   className: 'custom-popup-small',
   closeButton: false,
   closeOnClick: false
 });
+
+
+
 map.on("click","state-area",(e)=>{
-    const clickedFeature = e.features[0];
-
-    // Update the layer style to apply the raised effect
-    map.setPaintProperty('state-area', 'fill-extrusion-height', [
-      'match',
-      ['id'],
-      clickedFeature.id,
-      0.5,  // Height for the clicked state
-      0     // Default height for other states
-    ]);
-
-});
-map.on('mousemove', 'state-area', (e) => {
-  if (e.features.length > 0) {
-    var currentPolygonId = e.features[0].id;
-
-    if (hoveredPolygonId !== currentPolygonId) {
-      if (hoveredPolygonId !== null) {
-        map.setFeatureState(
-          { source: 'states', id: hoveredPolygonId },
-          { hover: false }
-        );
-        pp1.remove();
-      }
-
-      hoveredPolygonId = currentPolygonId;
-      map.setFeatureState(
-        { source: 'states', id: hoveredPolygonId },
-        { hover: true }
-      );
-
-      pp1.setLngLat(e.lngLat)
-        .setHTML(`<h4>${e.features[0].properties.STATE_NAME}</h4>`)
-        .addTo(map);
-    }
-  }
-});
-
-map.on('mouseleave', 'state-area', () => {
-  if (hoveredPolygonId !== null) {
-    map.setFeatureState(
-      { source: 'states', id: hoveredPolygonId },
-      { hover: false }
-    );
-    pp1.remove();
-  }
-  hoveredPolygonId = null;
-});
-
-map.on("dblclick","state-area",(e)=>{
   e.preventDefault() // to avaoid the default zoom behavior
   pp1.remove();
   pp2= new mapboxgl.Popup({
